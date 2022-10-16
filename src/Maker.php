bdf30213457442092fa42301a913e5ff9ee06764
Caimingjian <ilovecaimingjian@126.com>
2022-10-16 21:01:51 +0800

    完善返回

--- src/Maker.php
+++ src/Maker.php
@@ -2,19 +2,21 @@
 
 namespace cccaimingjian\ImageWatermark;
 
+use GdImage;
+
 class Maker
 {
-    protected int $w_interval   = 50;
-    protected int $h_interval   = 50;
-    protected float $str_angel    = 15.0;
-    protected int $color        = 0x30BEBEBE;
-    protected int $pic_w        = 0;
-    protected int $pic_h        = 0;
-    protected $image        = null;
-    protected string $font_path    = './font/SourceHanSerifSC-Medium.otf';
-    public string $file_path       = '';
-    public string $string          = 'watermark';
-    public float $font_size       = 10.0;
+    protected int $w_interval = 50;
+    protected int $h_interval = 50;
+    protected float $str_angel = 15.0;
+    protected int $color = 0x30BEBEBE;
+    protected int $pic_w = 0;
+    protected int $pic_h = 0;
+    protected ?GdImage $image = null;
+    protected string $font_path = './font/SourceHanSerifSC-Medium.otf';
+    public string $file_path = '';
+    public string $string = 'watermark';
+    public float $font_size = 10.0;
 
     public function __construct(string $original_path = '')
     {
@@ -27,46 +29,54 @@ class Maker
      * Set the image path (Original image)
      * 设置你要添加水印的那张图片的路径（原图）
      * @param string $file_path
+     * @return $this
      */
-    public function setInputFilePath(string $file_path)
+    public function setInputFilePath(string $file_path): Maker
     {
         $this->file_path = $file_path;
+        return $this;
     }
 
     /**
      * Set the angle of the watermark string
      * 设置水印字符的角度
      * @param float $angle
+     * @return $this
      * The angle in degrees, with 0 degrees being left-to-right reading text.
      * Higher values represent a counter-clockwise rotation.
      * For example, a value of 90 would result in bottom-to-top reading text.
      */
-    public function setAngle(float $angle)
+    public function setAngle(float $angle): Maker
     {
         $this->str_angel = $angle;
+        return $this;
     }
 
     /**
      * Set the font size of the watermark string
      * 设置水印字体大小
      * @param float $size
+     * @return $this
      * The font size. Depending on your version of GD, this should be specified as the pixel size (GD1) or point size (GD2).
      */
     public function setFontSize(float $size)
     {
         $this->font_size = $size;
+        return $this;
     }
 
     /**
      * Set the watermark string
      * 设置水印的内容
      * @param $string
-     * the string you want to add
+     * the string you want to add.
+     * @return $this
      *
      */
-    public function setWatermarkString(string $string)
+    public function setWatermarkString(string $string): Maker
     {
         $this->string = $string;
+        return $this;
     }
 
     /**
@@ -74,22 +84,26 @@ class Maker
      * 设置水印内容的颜色
      * @param int $color
      * The color index. Using the negative of a color index has the effect of turning off antialiasing. See imagecolorallocate.
+     * @return $this
      */
-    public function setWatermarkColor(int $color)
+    public function setWatermarkColor(int $color): Maker
     {
         $this->color = $color;
+        return $this;
     }
 
     /**
      * Set the interval of width
      * 设置水印横向的间隔
+     * 注意是开头的间隔，间隔大小请根据水印的字符长度合理设置
      * @param $interval int
      * the pixel between watermark's START in horizontal direction
-     * 注意是开头的间隔，间隔大小请根据水印的字符长度合理设置
+     * @return $this
      */
-    public function setWatermarkWidthInterval(int $interval)
+    public function setWatermarkWidthInterval(int $interval): Maker
     {
         $this->w_interval = $interval;
+        return $this;
     }
 
     /**
@@ -97,10 +111,12 @@ class Maker
      * 设置水印纵向的间隔
      * @param $interval
      * the pixel between watermark in vertical direction
+     * @return $this
      */
-    public function setWatermarkHeightInterval(int $interval)
+    public function setWatermarkHeightInterval(int $interval): Maker
     {
         $this->h_interval = $interval;
+        return $this;
     }
 
     /**
@@ -108,18 +124,20 @@ class Maker
      * 设置水印字体文件
      * @param $font_path
      * the font file path
+     * @return $this
      */
-    public function setWatermarkFont(string $font_path)
+    public function setWatermarkFont(string $font_path): Maker
     {
         $this->font_path = $font_path;
+        return $this;
     }
 
     /**
      * draw watermark
      * 画水印
-     * @throws Exception
+     * @throws \Exception
      */
-    public function drawWatermark()
+    public function drawWatermark(): Maker
     {
         if (!$this->image) {
             $this->loadPicture();
@@ -138,6 +156,7 @@ class Maker
                 );
             }
         }
+        return $this;
     }
 
     /**
@@ -183,20 +202,20 @@ class Maker
      * Load the image into Memory
      * 读取图片
      * @return bool
-     * @throws Exception
+     * @throws \Exception
      */
-    protected function loadPicture()
+    protected function loadPicture(): bool
     {
         if (!file_exists($this->file_path)) {
-            throw new Exception('File: "'.$this->file_path.'" does not exist');
+            throw new \Exception('File: "'.$this->file_path.'" does not exist');
         }
         $image_data = file_get_contents($this->file_path);
         if (!$image_data) {
-            throw new Exception('file_get_contents() can NOT get the file:"'.$this->file_path.'"');
+            throw new \Exception('file_get_contents() can NOT get the file:"'.$this->file_path.'"');
         }
         $this->image = imagecreatefromstring($image_data);
         if (!$this->image) {
-            throw new Exception('Can NOT Load the file:"'.$this->file_path.'", please confirm the file is an image');
+            throw new \Exception('Can NOT Load the file:"'.$this->file_path.'", please confirm the file is an image');
         }
         $this->pic_w = imagesx($this->image);
         $this->pic_h = imagesy($this->image);
@@ -213,9 +232,11 @@ class Maker
 
     /**
      * @param GdImage|null $image
+     * @return bool
      */
-    public function setImage($image): void
+    public function setImage(?GdImage $image): bool
     {
         $this->image = $image;
+        return true;
     }
 }
